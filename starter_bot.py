2f589533add6b6e662c4a1e61e7292adc59b2af3
Gaurav Kakoti <71715253+GauravKakoti@users.noreply.github.com>
2021-07-18 21:02:02 +0530

    Delete starter_bot.py

--- starter_bot.py
+++
@@ -1,9 +0,0 @@
-from chai_py import ChaiBot, Update
-
-
-class Bot(ChaiBot):
-    def setup(self):
-        self.logger.info("Setting up...")
-
-    async def on_message(self, update: Update) -> str:
-        return "I love AI"
